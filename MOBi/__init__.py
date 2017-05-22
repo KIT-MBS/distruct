e0137883497e4292dafc795cdcf3e4f16481176b
Oskar Taubert <uahcx@student.kit.edu>
2017-05-22 14:30:10 +0200

    some default alphabets added to init

--- MOBi/__init__.py
+++ MOBi/__init__.py
@@ -9,7 +9,7 @@
 #
 # Creation Date : Tue 09 May 2017 13:33:38 CEST
 #
-# Last Modified : Mon 22 May 2017 13:45:39 CEST
+# Last Modified : Mon 22 May 2017 14:28:17 CEST
 #
 #####################################
 
@@ -24,9 +24,14 @@ data_path = os.environ['MOBi_DATAPATH']
 gromacs_topology_path = '/usr/share/gromacs/top/'
 
 AAs = ['ALA', 'GLY', 'SER', 'THR', 'LEU', 'ILE', 'VAL', 'ASN', 'GLN', 'ARG', 'HIS', 'TRP', 'PHE', 'TYR', 'GLU', 'ASP', 'LYS', 'PRO', 'CYS', 'MET']
+NAs = ['A', 'G', 'C', 'U']
+
 # TODO put in translation for ff names and other conventions
 # NOTE amber knows HISD, HISE and HISH but not HIS (and calls them HID, HIE and HIP)
 amberAAs = ['ALA', 'GLY', 'SER', 'THR', 'LEU', 'ILE', 'VAL', 'ASN', 'GLN', 'ARG', 'HIP', 'TRP', 'PHE', 'TYR', 'GLU', 'ASP', 'LYS', 'PRO', 'CYS', 'MET']
 amberAAts = ['C' + AA for AA in amberAAs] + ['N' + AA for AA in amberAAs]
-# TODO
-RAs = []
+# TODO amberNAs
+
+# TODO gromos
+# TODO charmm
+
