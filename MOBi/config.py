9446826589f4d2c8ef0d096507373c3dd7445238
Oskar Taubert <oskar.taubert@kit.edu>
2018-05-15 17:43:34 +0200

    removed a print

--- MOBi/config.py
+++ MOBi/config.py
@@ -9,14 +9,13 @@
 #
 # Creation Date : Mon 22 May 2017 17:23:27 CEST
 #
-# Last Modified : Mon 30 Apr 2018 01:53:01 PM CEST
+# Last Modified : Wed 02 May 2018 04:09:30 PM CEST
 #
 #####################################
 
 import os
 
 data_path = os.path.split(os.path.abspath(__file__))[0] + '/../share/'
-print(data_path)
 
 # TODO do a setup function where it reads this from gromacs output
 # (or tries to  and if it does not work, warns and tells user to set it themselves)
