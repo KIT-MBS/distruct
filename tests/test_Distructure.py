8ede1d173a6314cd46bd086580abb7f679be8f05
Oskar Taubert <oskar.taubert@kit.edu>
2018-06-11 16:14:55 +0200

    added test file for distructure class

---
+++ tests/test_Distructure.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#####################################
+#
+# Filename : test_Distructure.py
+#
+# Projectname : MOBi
+#
+# Author : Oskar Taubert
+#
+# Creation Date : Mon 11 Jun 2018 04:04:59 PM CEST
+#
+# Last Modified : Mon 11 Jun 2018 04:14:04 PM CEST
+#
+#####################################
+
+def test_Distructure_init():
+    assert False
+    return
