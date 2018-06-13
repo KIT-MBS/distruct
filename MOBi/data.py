b61acabb81796b5b61408201fa37a2a1a9c022dc
Oskar Taubert <oskar.taubert@kit.edu>
2018-06-13 18:34:19 +0200

    added my own alphabets, that should cover most of what's in the PDB

--- MOBi/data.py
+++ MOBi/data.py
@@ -9,36 +9,53 @@
 #
 # Creation Date : Tue 15 Aug 2017 02:15:31 PM CEST
 #
-# Last Modified : Mon 28 May 2018 10:14:55 AM CEST
+# Last Modified : Wed 13 Jun 2018 06:07:22 PM CEST
 #
 #####################################
 
+from Bio.Alphabet import IUPAC
 from Bio import Alphabet
+from Bio.Data import IUPACData
 from math import pi
 # TODO handle alphabets here
 # use generic base stuff and IUPAC
 # {'ALA', 'Ala', 'A', 'a'}
 # extended
 
-AAs = ['ALA', 'GLY', 'SER', 'THR', 'LEU', 'ILE', 'VAL', 'ASN', 'GLN', 'ARG', 'HIS', 'TRP', 'PHE', 'TYR', 'GLU', 'ASP', 'LYS', 'PRO', 'CYS', 'MET']
-# RNAs = ['RA', 'RG', 'RC', 'RU']  # names as they appear in gromacs' amber99sb-ildn
-RNAs = ['A', 'G', 'C', 'U']  # names as they appear in the PDB
-DNAs = ['DA', 'DG', 'DC', 'DT']
+# PDBAAs = ['ALA', 'GLY', 'SER', 'THR', 'LEU', 'ILE', 'VAL', 'ASN', 'GLN', 'ARG', 'HIS', 'TRP', 'PHE', 'TYR', 'GLU', 'ASP', 'LYS', 'PRO', 'CYS', 'MET']
+# # RNAs = ['RA', 'RG', 'RC', 'RU']  # names as they appear in gromacs' amber99sb-ildn
+# PDBRNAs = ['A', 'G', 'C', 'U']  # names as they appear in the PDB
+# PDBDNAs = ['DA', 'DG', 'DC', 'DT']
 
+# NOTE Alphabets as they occur in PDB entries. Things like extended alphabets are not considered for now.
+# NOTE Ambiguities are not useful in this context. Extensions might be.
+AALetters = [IUPACData.protein_letters_1to3[letter].upper() for letter in IUPACData.protein_letters]
+DNALetters = ['D' + letter for letter in IUPACData.unambiguous_dna_letters]
+RNALetters = [letter for letter in IUPACData.unambiguous_rna_letters]
 
-class ReducedPDBProtein(Alphabet.ThreeLetterProtein):
-    letters = AAs
+class AAAlphabet(Alphabet.ThreeLetterProtein):
+    letters = AALetters
     pass
 
-
-class PDBRNA(Alphabet.RNAAlphabet):
-    letters = RNAs
+class DNAAlphabet(Alphabet.DNAAlphabet):
+    letters = DNALetters
+    size = 2
     pass
 
+class RNAAlphabet(Alphabet.RNAAlphabet):
+    letters = RNALetters
+    pass
 
-PDBReducedProtein = ReducedPDBProtein()
-PDBRNAalphabet = PDBRNA()
+class MoleculeAlphabet(Alphabet.Alphabet()):
+    letters = AALetters + DNALetters + RNALetters
+    pass
 
+AAalphabet = AAAlphabet()
+DNAalphabet = DNAAlphabet()
+RNAalphabet = RNAAlphabet()
+alphabet = MoleculeAlphabet()
+
+# TODO secondary structure
 
 # TODO handle terminals (C,N,3', and 5')
 # TODO put in translation for ff names and other conventions
@@ -46,10 +63,6 @@ PDBRNAalphabet = PDBRNA()
 # NOTE amber knows HISD, HISE and HISH but not HIS (and calls them HID, HIE and HIP)
 # amberAAs = ['ALA', 'GLY', 'SER', 'THR', 'LEU', 'ILE', 'VAL', 'ASN', 'GLN', 'ARG', 'HIP', 'TRP', 'PHE', 'TYR', 'GLU', 'ASP', 'LYS', 'PRO', 'CYS', 'MET']
 # amberAAts = ['C' + AA for AA in amberAAs] + ['N' + AA for AA in amberAAs]
-# TODO amberNAs
-
-# TODO pdb alphabet
-# TODO pdb extended alphabet
 
 # NOTE default values to determine edges for secondary structure
 # TODO improve and cite these
@@ -62,8 +75,5 @@ hangle = 180. * pi / 180.  # angle donor-hydrogen-acceptor
 
 # TODO default omega value
 
-# TODO maybe need nucleic acid specific stuff
-
-# TODO put in alphabet with AAs and NAs
 from .fileio import read_topology_file
-defaultTopologyDB = read_topology_file("amber99sb-ildn_protein")
+defaultTopologyDB = read_topology_file("amber99sb-ildn")
