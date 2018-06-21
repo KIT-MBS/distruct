28413369ec081e53909825adc475eba36a82b8a6
Oskar Taubert <oskar.taubert@kit.edu>
2018-06-21 10:00:41 +0200

    changed test_Distructure to be minimal test

--- tests/test_Distructure.py
+++ tests/test_Distructure.py
@@ -9,33 +9,41 @@
 #
 # Creation Date : Mon 11 Jun 2018 04:04:59 PM CEST
 #
-# Last Modified : Wed 20 Jun 2018 01:50:36 PM CEST
+# Last Modified : Wed 20 Jun 2018 04:38:19 PM CEST
 #
 #####################################
 
+from Bio import SeqIO
+from Bio.PDB.PDBParser import PDBParser
+
 from MOBi import Distructure
 from MOBi import data
+from MOBi import config
+from MOBi import Superimposer
+
+
+testFilePath = config.data_path + "tests/"
 
 
 def test_Distructure():
 
     code = "1ptq"
-    fileName = code + '.pdb'
+    fileName = testFilePath + code + '.pdb'
 
-    # generate distruct
-    # create structure
+    sequences = list()
+    with open(fileName, 'rU') as f:
+        for record in SeqIO.parse(f, "pdb-seqres"):
+            print(record)
+            sequences.append(record.seq)
+            pass
+        pass
+    # TODO use only one letter alphabets and choose building block based on the alphabet
+
+    # create distruct
     ds = Distructure(code, sequences)
-    # create edges
+
     # create primary edges
     ds.generate_primary_edges()
-    # create tertiary edges
-    # get contact map
-    contactMap = MOBi.tools.generate_atomic_contacts(refStructure)
-    ds.generate_tertiary_edges(contactMap)
     # generate coordinates
     ds.run()
-
-    # TODO structural alignment
-    RMSD = MOBi.tools.align(ds, refStructure)
-
     return
