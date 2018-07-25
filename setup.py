502daf5b90ae5fd70f4252763daab56dd28981ce
Oskar Taubert <oskar.taubert@kit.edu>
2018-07-25 17:35:45 +0200

    compile works now. has to get cleaned up.

--- setup.py
+++ setup.py
@@ -9,12 +9,10 @@
 #
 # Creation Date : Thu 28 Jun 2018 12:50:34 PM CEST
 #
-# Last Modified : Tue 03 Jul 2018 07:48:29 PM CEST
+# Last Modified : Wed 25 Jul 2018 05:08:13 PM CEST
 #
 #####################################
 
-# NOTE for now i use distutils. maybe have to switch to setuptools?
-# TODO better way to find the networkit c++ headers?
 # TODO look at what kind of structure/files pypi expects
 # TODO install requirements
 
@@ -30,23 +28,29 @@ def networkit_get_include():
     return
 
 sources = ['MOBi/src/BioMaxentStress.cpp', "MOBi/_MOBi" + fext]
-# TODO download nwk c++ headers
+
+# TODO correct include path
 from os import environ
 includeDirs = [environ["HOME"] + "/Projects/networkit/networkit/", "MOBi/src/"]
-libraryDirs = ["/usr/lib/python3.6/site-packages/"]
-# TODO fix this
-libraries = [libraryDirs[0] + "_NetworKit.cpython-36m-x86_64-linux-gnu.so"]
-# TODO try global and local, use local first
 
-# TODO optimization
+# TODO determine lib path from networkit install path
+libraryDirs = ["/usr/lib/python3.6/site-packages/"]
+
+# TODO determine lib name at install time
+libraries = [":_NetworKit.cpython-36m-x86_64-linux-gnu.so"]
+
+compile_args = ["-fopenmp"]
+link_args = ["-fopenmp"]
+
+# TODO basically use the pythonpath as LD_LIBRARY_PATH
 
 extensions = [
         Extension(
             "_MOBi",
             sources,
             language = "c++",
-            extra_compile_args = ["-fopenmp"],
-            extra_link_args = ["-fopenmp"],
+            extra_compile_args = compile_args,
+            extra_link_args = link_args,
             include_dirs = includeDirs,
             libraries = libraries,
             library_dirs = libraryDirs)
