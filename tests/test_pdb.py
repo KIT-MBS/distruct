21b6ee5a7a57c65842fa2b51d45061e2168e3f66
Oskar Taubert <oskar.taubert@kit.edu>
2018-08-10 16:37:08 +0200

    renamed pdb test script

--- tests/test_pdb.py
+++ tests/test_pdb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #####################################
 #
-# Filename : test_pdbparser.py
+# Filename : test_pdb.py
 #
 # Projectname : MOBi
 #
@@ -9,7 +9,7 @@
 #
 # Creation Date : Mon 30 Apr 2018 05:13:44 PM CEST
 #
-# Last Modified : Thu 02 Aug 2018 01:40:26 PM CEST
+# Last Modified : Fri 10 Aug 2018 04:35:50 PM CEST
 #
 #####################################
 
@@ -17,3 +17,5 @@
 # TODO test RNA and protein and DNA and mixed complexes
 # TODO test parse xray and nmr generated structures
 # TODO test all functions
+
+
